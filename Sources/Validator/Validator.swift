8224c22873f050828b01538f285fefca6f3bad28
Igor Malyarov <igor1309@gmail.com>
2023-03-14 22:59:58 +0300

    protocol `Validator`

--- Sources/Validator/Validator.swift
+++ Sources/Validator/Validator.swift
@@ -1,6 +1,45 @@
-public struct swift_validator {
-    public private(set) var text = "Hello, World!"
+//
+//  Validator.swift
+//
+//
+//  Created by Igor Malyarov on 14.03.2023.
+//
 
-    public init() {
-    }
+#if swift(>=5.7)
+public protocol Validator<Input, Failure> {
+    
+    /// The type of values this validator can validate.
+    associatedtype Input
+    
+    /// The type of failures produced by this validator.
+    associatedtype Failure
+    
+    /// Attempts to validate a piece of data producing failure.
+    /// Typically
+    /// you only call this from other `Parser` conformances, not when you want to parse a concrete
+    /// input.
+    ///
+    /// - Parameter input: A piece of data to be validated.
+    /// - Returns: Result of validation, either Void in case of successful validation, or failure other.
+    func validate(_ input: Input) -> ValidatorResult<OK, Failure>
 }
+#else
+public protocol Validator {
+    
+    associatedtype Input
+    associatedtype Failure
+    
+    func validate(_ input: Input) -> ValidatorResult<OK, Failure>
+}
+#endif
+
+public struct OK: Equatable {
+    public init() {}
+}
+
+public enum ValidatorResult<Success, Failure> {
+    case valid(Success)
+    case failure(Failure)
+}
+
+extension ValidatorResult: Equatable where Success: Equatable, Failure: Equatable {}
