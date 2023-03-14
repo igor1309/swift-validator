7a0421038a3c438683b8b70021bf252cd5d639b5
Igor Malyarov <igor1309@gmail.com>
2023-03-14 23:03:09 +0300

    `ChainValidator`

---
+++ Tests/ValidatorTests/ChainValidatorTests.swift
@@ -0,0 +1,16 @@
+//
+//  ChainValidatorTests.swift
+//  
+//
+//  Created by Igor Malyarov on 14.03.2023.
+//
+
+import XCTest
+
+class ChainValidatorTests: XCTestCase {
+
+    func unimplemented() {
+        
+        XCTFail("Unimplemented")
+    }
+}
