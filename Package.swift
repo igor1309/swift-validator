7ad8e4c91dcbff9ee635c44ed35a6644f60eb7af
Igor Malyarov <igor1309@gmail.com>
2023-03-14 20:33:32 +0300

    cleanup

--- Package.swift
+++ Package.swift
@@ -1,28 +1,47 @@
-// swift-tools-version: 5.7
-// The swift-tools-version declares the minimum version of Swift required to build this package.
+// swift-tools-version: 5.4
 
 import PackageDescription
 
 let package = Package(
     name: "swift-validator",
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
-        .library(
-            name: "swift-validator",
-            targets: ["swift-validator"]),
-    ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
+        .validator,
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
-        .target(
-            name: "swift-validator",
-            dependencies: []),
-        .testTarget(
-            name: "swift-validatorTests",
-            dependencies: ["swift-validator"]),
+        .validator,
+        .validatorTests,
     ]
 )
+
+private extension Product {
+    
+    static let validator = library(
+        name: .validator,
+        targets: [
+            .validator
+        ]
+    )
+}
+
+private extension Target {
+    
+    
+    static let validator = target(name: .validator)
+    static let validatorTests = testTarget(
+        name: .validatorTests,
+        dependencies: [
+            .validator
+        ]
+    )
+}
+
+private extension Target.Dependency {
+    
+    static let validator = byName(name: .validator)
+}
+
+private extension String {
+    
+    static let validator = "Validator"
+    static let validatorTests = "ValidatorTests"
+}
