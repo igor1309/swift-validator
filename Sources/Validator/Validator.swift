5d8a464a2891639b4c57a09d9954e5b37d79ef89
Igor Malyarov <igor1309@gmail.com>
2023-04-02 20:49:00 +0300

    tweak docs

--- Sources/Validator/Validator.swift
+++ Sources/Validator/Validator.swift
@@ -15,9 +15,8 @@ public protocol Validator<Input, Failure> {
     associatedtype Failure
     
     /// Attempts to validate a piece of data producing failure.
-    /// Typically
-    /// you only call this from other `Parser` conformances, not when you want to parse a concrete
-    /// input.
+    /// Typically you only call this from other `Validator` conformances,
+    /// not when you want to validate a concrete input.
     ///
     /// - Parameter input: A piece of data to be validated.
     /// - Returns: Result of validation, either Void in case of successful validation, or failure other.
@@ -29,6 +28,11 @@ public protocol Validator {
     associatedtype Input
     associatedtype Failure
     
+    /// - Note: Alternative considered is `Optional<Failure>` where
+    /// `Optional<Failure>.none` would represent valid result,
+    /// but that does not feel right, although technically we don't really care
+    /// what exactly represents success validation (except for the tests
+    /// where it would be good to have some `Equatable` type).
     func validate(_ input: Input) -> ValidatorResult<OK, Failure>
 }
 #endif
